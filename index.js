0fd1f303ab39b9a0cd294872129de3fd0a722132
Xavier Choudhury <xchoudhury@gmail.com>
2017-07-02 00:25:11 -0400

    added test to env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 8000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World! This is my first node app!')
+  res.send('Hello World! This is my first node app! ' + process.env.TEST)
 })
 
 app.listen(app.get('port'), function () {

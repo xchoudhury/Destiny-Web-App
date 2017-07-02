337f8524ec8fea23ad904b7e1e27c766371c4efb
Xavier Choudhury <xchoudhury@gmail.com>
2017-07-02 00:08:15 -0400

    uses environment port now

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.set('port', (process.env.PORT || 8000));
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!')
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
 })
